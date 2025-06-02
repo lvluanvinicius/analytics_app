79c67c308318443cd0e659ffe5e78ac2547405a4
Luan Santos <lvluansantos@gmail.com>
2025-06-02 10:23:46 -0300

    DEV-129: configurado rotas de interligação.

---
+++ app/Http/Controllers/Api/V1/InterconnectionController.php
@@ -0,0 +1,82 @@
+<?php
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\Controller;
+use App\Zabbix\Models\Hosts;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class InterconnectionController extends Controller
+{
+    /**
+     * Recupera dados para mapa de interligação.
+     *
+     * @author Luan Santos <lvluansantos@gmail.com>
+     *
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function index(Request $request): JsonResponse
+    {
+        try {
+            // Verificando se o token foi informado corretamente.
+            if (! $request->headers->has('zabbixtoken')) {
+                throw new \Exception("Por favor, informe o token do zabbix no header com a chave 'zabbixtoken'.");
+            }
+
+            // Verificando se o endereço do zabbix foi informado.
+            if (! $request->has('zabbixlocation')) {
+                throw new \Exception("Por favor, o parâmetro 'zabbixlocation' é obrigatório.");
+            }
+
+            // Verificando se o groupids foi informado.
+            if (! $request->has('groupids')) {
+                throw new \Exception("Por favor, o parâmetro 'groupids' é obrigatório.");
+            }
+
+            // Recuperando token do zabbix.
+            $zabbixToken = $request->headers->get('zabbixtoken');
+
+            // Recuperando local|endereço do zabbix.
+            $baseUrl = $request->query('zabbixlocation');
+
+            //
+            $groupId = $request->get('groupids');
+
+            // Criando conexão com a API.
+            $zabbix = new Hosts(
+                $urlbase = $baseUrl,
+                $token = $zabbixToken
+            );
+
+            $zabbixData = $zabbix->request([
+                "jsonrpc" => "2.0",
+                "method"  => "host.get",
+                "id"      => 1,
+                "params"  => [
+                    "output"          => ["host", "name"],
+                    "groupids"        => explode(',', $groupId),
+                    "selectItems"     => ["lastvalue", "name"],
+                    "selectInventory" => ["location_lat", "location_lon", "notes", "location", "contact"],
+                    "limit"           => 0,
+                    "offset"          => 0,
+                ],
+            ]);
+
+            // Novo array para retorno com os dados separados.
+            $newHosts = [];
+            foreach ($zabbixData['result'] as $zbxData) {
+                array_push($newHosts, [
+                    "name"      => $zbxData['name'],
+                    "host"      => $zbxData['host'],
+                    "location"  => $zbxData['inventory'],
+                    "lastvalue" => $zbxData["items"],
+                ]);
+            }
+
+            return $this->successResponse($newHosts, 'Hosts recuperados com sucesso.');
+        } catch (\Exception $error) {
+            return $this->errorResponse($error->getMessage(), \Illuminate\Http\Response::HTTP_BAD_REQUEST);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\V1\InterconnectionController;
+use App\Http\Controllers\Api\V1\PopsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->as('v1')->group(function () {
+    Route::prefix('interconnection')->as('interconnection.')->group(function () {
+        Route::get('/', [InterconnectionController::class, 'index'])->name('index');
+    });
+
+    Route::prefix('pops')->as('pops.')->group(function () {
+        Route::get('/', [PopsController::class, 'index'])->name('index');
+    });
+});
